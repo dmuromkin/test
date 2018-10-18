a23cf2294801438855ecbeef36694014ca930c73
Dmitriy <muromkin1997@gmail.com>
2018-10-18 13:01:44 +0300

    Added_main2

---
+++ OxyPlotTest/Integral.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlotTest
+{
+    class Integral
+    {
+        public static double Calculation()
+        {
+            double h = 0.01;
+            int a = 1;
+            int b = 10000;
+            int N = (int)((b - a) / h);
+           // double x = a + h;
+            object monitor = new object();
+            double integral = 0.0;/*
+            for (int i = 0; i < N; i++)
+            {
+                integral += h * func(x - h / 2);
+                x += h;
+            }*/
+            Parallel.For(0, N, () => 0.0, (i, state, local) =>
+            {
+                double x = a + (i+1)*h;
+                local += h * func(x - h / 2);
+                return local;
+
+            }, local => { lock (monitor) integral += local; });
+            
+            return integral*h;
+        }
+        /*
+          static public double Trapeze_parallel(double a, double b, double h)
+        {
+            double S;
+            // calculate number of steps
+            int N = (int)((b - a) / h);
+            // object for lock mutex and synchronize threads
+            object monitor = new object();
+            S = (F11(a) + F11(b)) / 2;
+
+            // Every thread have own local data, 
+            // and thread calculate own part of integral
+            // then it sum in main thread with S+= local
+            Parallel.For(0, N, () => 0.0, (i, state, local) =>
+            {
+               
+                local += F11(a + h*i);
+                return local;
+            }, local => { lock (monitor) S += local; });
+
+            S *= h;
+            return S;
+        }
+             */
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        /*
+        Parallel.For(0, N, () => 0.0, (i, state, local) =>
+           {
+               local += h * func(x - h / 2);
+               x += h;
+               return local;
+
+           }, local => { lock (monitor) integral += local; });*/
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        static double func(double x)
+        {
+            return 7 * x - Math.Log(7 * x) + 8;
+        }
+    }
+}
---
+++ OxyPlotTest/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OxyPlotTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = Integral.Calculation().ToString();
+           
+            (graph1.Model.Series[0] as FunctionSeries).Points.Clear();
+            (graph1.Model.Series[0] as FunctionSeries).Points.Add(new DataPoint(10, 10));
+            (graph1.Model.Series[0] as FunctionSeries).Points.Add(new DataPoint(20, 20));
+            graph1.InvalidatePlot();
+
+           //(graph1.Model.Series[1] as BarSeries).Items.Add(new BarItem(3));
+        }
+    }
+}
